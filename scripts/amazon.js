f942d40b2d655187b4fcaaa0b02b236e6e0a1a8c
MrItzreal <is.wsie@gmail.com>
2024-02-17 07:42:02 -0500

    I used template strings and the accumulator pattern to optimized HTML generation, adding items to the output on each loop.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -32,29 +32,37 @@ const products = [
   },
 ];
 
+//Accumulator pattern.
+let productsHTML = "";
+
 products.forEach((product) => {
-  const html = `
+  //since JS does not do well with decimals we divide the,
+  //whole price by 100 so we can get the exact merch price.
+
+  productsHTML += `
   <div class="product-container">
   <div class="product-image-container">
     <img
       class="product-image"
-      src="images/products/athletic-cotton-socks-6-pairs.jpg"
+      src="${product.image}"
     />
   </div>
 
   <div class="product-name limit-text-to-2-lines">
-    Black and Gray Athletic Cotton Socks - 6 Pairs
+    ${product.name}
   </div>
 
   <div class="product-rating-container">
     <img
       class="product-rating-stars"
-      src="images/ratings/rating-45.png"
+      src="images/ratings/rating-${product.rating.stars * 10}.png"
     />
-    <div class="product-rating-count link-primary">87</div>
+    <div class="product-rating-count link-primary">${product.rating.count}</div>
   </div>
 
-  <div class="product-price">$10.90</div>
+  <div class="product-price">
+  $${product.priceCents / 100}
+  </div>
 
   <div class="product-quantity-container">
     <select>
@@ -81,6 +89,6 @@ products.forEach((product) => {
   <button class="add-to-cart-button button-primary">Add to Cart</button>
 </div>
 `;
-
-  console.log(html);
 });
+
+console.log(productsHTML);
