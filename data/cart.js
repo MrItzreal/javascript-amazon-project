83663c8b80d599c62b6d546e480dfb7cc6c26d57
MrItzreal <is.wsie@gmail.com>
2024-02-19 10:23:52 -0500

    Moved addToCart into cart.js.

--- data/cart.js
+++ data/cart.js
@@ -1,2 +1,22 @@
 //export lets us use variables outside of their files.
 export const cart = [];
+
+export function addToCart(productId) {
+  //checks if product is already in cart.
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  }); //if product already in cart it increases the quantity.
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } //if not in cart, then it adds to cart.
+  else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
