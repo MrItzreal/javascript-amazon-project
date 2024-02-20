eabff831829993f914078f51342f27a9b49641d4
MrItzreal <is.wsie@gmail.com>
2024-02-20 13:34:41 -0500

    Save cart in localStorage.

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,30 @@
-export let cart = [
-  //Data deduplication or normalizing data is a technique for eliminating redundant,
-  //copies of data essentially minimizing storage space and bandwidth usage.
-  //using the product ID we get the other values without having to retype them.
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+//.getItem() takes only one string.
+//JSON.parse() converts strings into objects or values.
+
+if (!cart) {
+  cart = [
+    //Data deduplication or normalizing data is a technique for eliminating redundant,
+    //copies of data essentially minimizing storage space and bandwidth usage.
+    //using the product ID we get the other values without having to retype them.
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
+
+function saveToStorage() {
+  //.setItem() takes two strings:
+  //1:Name of what you want to save.
+  //2:Data you want to save.
+  //LocalStorage only saves strings so we use JSON.stringify to convert to strings.
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   //checks if product is already in cart.
@@ -31,6 +45,7 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -43,5 +58,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
- 
